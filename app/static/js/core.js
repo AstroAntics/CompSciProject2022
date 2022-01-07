c6c2fb13dd9f9581bb0aac34096f181ef82c1d24
SilverCrown <vucinicm@asbindia.org>
2022-01-07 12:26:47 +0530

    Images and new site layout

--- app/static/js/core.js
+++ app/static/js/core.js
@@ -1,39 +1,23 @@
 $(document).ready(function() {
     console.log("Page loaded successfully.")
 
-    // google maps page: show location of restaurants near user on homepage
-//    make_restaurant_map()
+    $('#dropdownMenu1').click(function (e) {
+        e.preventDefault();
+        console.log(e);
 
-//    $("#restaurant_location_map").append(mapsFrame)
+        _restaurantsDropdown =  $('#restaurantsDropdown');
+        _restaurantChildren = _restaurantsDropdown.children();
+        _restaurantParent = _restaurantChildren.parent();
 
-    // create restaurant map using api key
-//    function make_restaurant_map(api_key) {
-//        console.log("Creating Google Maps page.")
-//        var mapsFrame = document.createElement('iframe');
-//        $("mapsFrame").attr({
-//            "width": '650px',
-//            "height": '450px',
-//            "style": "border: 0",
-//            "loading": "lazy",
-//            "src": "https://www.google.com/maps/embed/v1/place?key=" + api_key + "&q=Space+Needle,Seattle+WA"
-//        });
-//
-//        return mapsFrame
-//    }
-
-$('#dropdownMenu1').click(function (e) {
-    e.preventDefault();
-    console.log(e);
-
-//    _children = $('#dropdownMenu1').children();
-//    console.log(_children)
-
-    _restaurantsDropdown =  $('#restaurantsDropdown');
-//    console.log(_restaurantsDropdown)
-    _restaurantChildren = _restaurantsDropdown.children();
-    _restaurantChildren.parent().attr('style' , 'display: block');
-//    console.log(_restaurantChildren);
-//     _restaurantChildren.attr('display' , 'block');
-})
-
+        // Extend or retract dropdown based on user interaction
+        if (_restaurantParent.hasClass('extended')) {
+            console.log("Collapsing dropdown.");
+            _restaurantParent.removeClass('extended');
+            _restaurantParent.attr('style' , 'display: none');
+        } else {
+            console.log("Extending dropdown.")
+            _restaurantParent.attr('style' , 'display: inline-block');
+            _restaurantParent.addClass('extended');
+        }
+    });
 });
